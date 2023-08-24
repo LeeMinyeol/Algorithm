60ae66c517b74edce0a261139331fe4c89b42d04
LeeMinyeol <54468674+LeeMinyeol@users.noreply.github.com>
2023-08-24 12:25:54 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.15 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public string solution(int num) {
+        string answer = "";
+
+        if(num % 2 == 0) {
+            answer = "Even";
+        } else {
+            answer = "Odd";
+        }
+        
+        return answer;
+    }
+}
