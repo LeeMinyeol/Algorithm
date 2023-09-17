fdfae574625de8df4ed75bda583a6e99da31a2a3
LeeMinyeol <54468674+LeeMinyeol@users.noreply.github.com>
2023-09-18 00:40:44 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.19 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public string solution(string phone_number) {
+        string answer = "";
+        answer = phone_number.Substring(phone_number.Length - 4);
+        answer = answer.PadLeft(phone_number.Length, '*');
+        
+        return answer;
+    }
+}
