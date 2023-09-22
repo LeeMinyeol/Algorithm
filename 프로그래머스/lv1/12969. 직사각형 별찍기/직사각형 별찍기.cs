4c559a62fdf4175383f25200c62237df6576a0e7
LeeMinyeol <54468674+LeeMinyeol@users.noreply.github.com>
2023-09-22 09:40:11 +0900

    [level 1] Title: 직사각형 별찍기, Time: 61.50 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] s;
+        string answer;
+        Console.Clear();
+        s = Console.ReadLine().Split(' ');
+
+        int a = Int32.Parse(s[0]);
+        int b = Int32.Parse(s[1]);
+        
+        for(int i = 0; i < b; i++) {
+            for(int j = 0; j < a; j++) {
+                Console.Write("*");
+            }
+            Console.Write("\n");
+        }
+
+    }
+}
