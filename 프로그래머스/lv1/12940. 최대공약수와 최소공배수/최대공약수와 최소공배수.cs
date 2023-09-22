050b57593252031c0857726d2844f3cde742cc73
LeeMinyeol <54468674+LeeMinyeol@users.noreply.github.com>
2023-09-22 09:49:22 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.29 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        int min;
+        int max;
+        
+        min = gcd(n, m);
+        max = (n*m) / min;
+        answer[0] = min;
+        answer[1] = max;
+                
+        return answer;
+    }
+    
+    public int gcd(int n, int m) {
+        if(n%m == 0) {
+            return m;
+        } else {
+            return gcd(m, n%m);
+        }
+        
+    }
+}
